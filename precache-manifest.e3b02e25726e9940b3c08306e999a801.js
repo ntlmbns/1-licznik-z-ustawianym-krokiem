cee0d36744c85ebea37a272779e976f8ad9ee397
ntlmbns <57600457+ntlmbns@users.noreply.github.com>
2020-01-30 22:56:48 +0100

    Updates

---
+++ precache-manifest.e3b02e25726e9940b3c08306e999a801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e3881a93867b108558e1966ac803e9",
+    "url": "/1-licznik-z-ustawianym-krokiem-2.git/index.html"
+  },
+  {
+    "revision": "1ea6a95d16e004db688b",
+    "url": "/1-licznik-z-ustawianym-krokiem-2.git/static/css/main.295984a9.chunk.css"
+  },
+  {
+    "revision": "deb49755ff55abe4dbd6",
+    "url": "/1-licznik-z-ustawianym-krokiem-2.git/static/js/2.2c84f1c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/1-licznik-z-ustawianym-krokiem-2.git/static/js/2.2c84f1c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ea6a95d16e004db688b",
+    "url": "/1-licznik-z-ustawianym-krokiem-2.git/static/js/main.ad8d0d7f.chunk.js"
+  },
+  {
+    "revision": "4c90db49472390b51757",
+    "url": "/1-licznik-z-ustawianym-krokiem-2.git/static/js/runtime-main.6fbf4db2.js"
+  }
+]);
--- src/CounterDisplay.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'; 
-
-const counterDisplay = props => { 
-    return (<p>Stan licznika: {props.counterCurrentValue}</p>);
-}
-
-export default counterDisplay; 
