c81bc951c8f6ba73a074a8604c47b9a4c24923c8
ntlmbns <57600457+ntlmbns@users.noreply.github.com>
2020-01-30 22:33:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Heading from './Heading';
+import Counter from './Counter'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading text={'Licznik w React'} classValue='main-heading' />
       </header>
+      <Counter />
     </div>
   );
 }
